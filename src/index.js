62912af31345b29fe7f6d7f3091411f9b18f7c08
Lilian Francis <ifnotlily@yahoo.com>
2021-11-13 14:45:00 +0100

    Update Javascript files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { Provider } from 'react-redux';
-import store from './redux/configStore';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Router>
       <App />
-    </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
